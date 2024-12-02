391279a26d367a5d4dac647a22051d77d18278b3
AnLaxina <anilovlaxina@gmail.com>
2024-12-01 23:13:23 -0800

    Finish exercise 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (word, numOfTimes) {
+    let wordToReturn = "";
 
+    if (numOfTimes < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < numOfTimes; i++) {
+        wordToReturn += word;
+    }
+
+    return wordToReturn;
 };
 
 // Do not edit below this line

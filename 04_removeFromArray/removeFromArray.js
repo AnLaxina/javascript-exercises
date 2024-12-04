07d922725cabc77b012835495d8fb5f07a0bb977
AnLaxina <anilovlaxina@gmail.com>
2024-12-03 21:56:15 -0800

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...elementsToRemove) {
+    // To handle multiple values/elements, I use the rest parameter
+    // to remove any number of elements in the array (if they exist)
+    // for (const element of elementsToRemove) {
+    //     const elementIndex = arr.indexOf(element);
+    //     arr.splice(elementIndex, 2);
+    // }
+
+    // return arr
+    return arr.filter(val => !elementsToRemove.includes(val));
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

495a34aeed86ed01771a6b753f12143e6bd07c81
AnLaxina <anilovlaxina@gmail.com>
2024-12-05 23:02:27 -0800

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let sum = 0;
+    // Ensures that the given parameters are valid (positive integers)
+    if (Number.isInteger(start) && Number.isInteger(end) && start >= 0 && end >= 0) {
+        if (start > end) {
+            for (; start >= end; start--) {
+                sum += start;
+            }
+        }
+        else {
+            for (; start <= end; start++) {
+                sum += start;
+            }
+        }
+
+        return sum;
+    }
+    return "ERROR";
 
 };
 

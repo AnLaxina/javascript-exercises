98751ba20efa1310595eb4faea03d9fdac93ccac
AnLaxina <anilovlaxina@gmail.com>
2024-12-05 23:13:02 -0800

    Finish exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // Criteria for leap year:
+    // - Divisible by 4
+    // - Can't be divisible by 100 unless also divisible by 400
+    // (year % 100 === 0 && year % 400 === 0) && year % 100 != 0
+
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 
 };
 

650275410c8f5ebb715a22f2f09d3479d5f81ba3
AnLaxina <anilovlaxina@gmail.com>
2024-12-09 12:47:35 -0800

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+// To convert from fahrenheit to celsius the formula is:
+// (F - 32) * 5/9 = Celsius
+const convertToCelsius = function (fahrenheitTemp) {
+  let celsius = (fahrenheitTemp - 32) * 5 / 9;
+  // I multiply the initial value by 10 to get a "big" number
+  // then, divide it by 10 to get the decimal point by 1
+  let converted = Math.round(celsius * 10) / 10;
+  return converted;
 };
 
-const convertToFahrenheit = function() {
+// Converting from celsius to fahrenheit is kind of similar:
+// (C * 9/5) + 32 = Fahrenheit
+const convertToFahrenheit = function (celsiusTemp) {
+  let fahrenheit = (celsiusTemp * 9 / 5) + 32;
+  let converted = Math.round(fahrenheit * 10) / 10;
+  return converted;
 };
 
 // Do not edit below this line

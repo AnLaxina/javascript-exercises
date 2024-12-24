967e89d967726a5113b7b09cf7218a101d3e86e7
AnLaxina <anilovlaxina@gmail.com>
2024-12-24 12:06:51 -0800

    Finish working solution for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,21 @@
 const findTheOldest = function (people) {
+    const noDeadPeople = people.map((person) => typeof person.yearOfDeath === "undefined");
+    const combinedPeople = people.concat(noDeadPeople);
     // Sorts each person's age from oldest to youngest
-    people.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
-    return people[0];
+    combinedPeople.sort((a, b) => {
+        const date = new Date();
+        let currentYear = date.getFullYear();
+        if (typeof a.yearOfDeath === "undefined") {
+            return (b.yearOfDeath - b.yearOfBirth) - (currentYear - a.yearOfBirth);
+        }
+        else if (typeof b.yearOfDeath === "undefined") {
+            return (currentYear - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+
+        }
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+
+    });
+    return combinedPeople[0];
 };
 
 // Do not edit below this line

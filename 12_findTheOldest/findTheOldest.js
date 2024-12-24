6c4f2e5bcbcecb38a6bc7f970631084fd7546845
AnLaxina <anilovlaxina@gmail.com>
2024-12-23 21:00:13 -0800

    Finish function to pass 1st case

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    // Sorts each person's age from oldest to youngest
+    people.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+    return people[0];
 };
 
 // Do not edit below this line

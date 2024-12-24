09d9ed532eb61a8fccc008adb3159b08eedc49aa
AnLaxina <anilovlaxina@gmail.com>
2024-12-23 20:10:09 -0800

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    // For this, I'm removing any characters in the string that are NOT letters
+    // this makes it easier to compare words later
+    let newWord = word.replace(/\W/g, '').toLowerCase();
+    let reversedWord = newWord.split("").reverse("").join("");
+    return newWord === reversedWord;
 
 };
 

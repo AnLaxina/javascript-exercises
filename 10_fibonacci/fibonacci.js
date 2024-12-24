00fe117e1c8fc37cd504ccc6bda4f7f77d1d3e42
AnLaxina <anilovlaxina@gmail.com>
2024-12-23 20:33:30 -0800

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (place) {
+    let n1 = 0;
+    let n2 = 1;
+    let nextTerm;
 
+    if (place < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 1; i <= place; i++) {
+        console.log(n1);
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+
+    return n1;
 };
 
 // Do not edit below this line
